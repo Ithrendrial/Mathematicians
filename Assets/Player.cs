1e17052a45aaa634d099e60db9b8f8f45ae3d476
Ithrendrial <nsx9698@autuni.ac.nz>
2024-08-17 15:53:29 +1200

    feat: add basic player movement

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
+using UnityEditor.Callbacks;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float speed = 20.0f;
+    public Vector2 direction;
+    private Rigidbody2D _rb;
+    private SpriteRenderer _sr;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
+        _sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float x_move = 0.0f;
+        float y_move = 0.0f;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            y_move = 1.0f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            y_move = -1.0f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            x_move = -1.0f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            x_move = 1.0f;
+        }
+
+        direction = new Vector2(x_move, y_move).normalized;
+        _rb.velocity = speed * direction * Time.deltaTime;
     }
 }
