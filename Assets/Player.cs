6296fe89e40109ebb4130ced51e8a157f83409bd
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2024-08-17 18:16:20 +1200

    add basic dialog

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // Basic Movement Variables
+    public float moveSpeed = 5f;
+
+    // Top Down Movement Variables
+    private Vector2 movement;
+    private Rigidbody2D rb;
+    private Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Input
+        GetInput();
         
+        Move();
+
+        if (animator != null)
+        {
+            Animate();
+        }
+    }
+    
+    void GetInput()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+    
+    
+    void Move()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+
+    void Animate()
+    {
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 }
