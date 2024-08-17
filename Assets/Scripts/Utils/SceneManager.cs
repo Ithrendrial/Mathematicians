a5e623de9ee90602a154caaee053f5f7fde1c50c
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2024-08-17 15:48:28 +1200

    add some basic utils

---
+++ Assets/Scripts/Utils/SceneManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+
+
+public class SceneManager : StaticInstance
+    {
+        public void ChangeCurrentScene(string sceneName) => UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+
+        public void ChangeCurrentScene(int sceneBuildIndex) => UnityEngine.SceneManagement.SceneManager.LoadScene(sceneBuildIndex);
+
+        public static void ReloadCurrentScene()
+        {
+            string currentSceneName = UnityEngine.SceneManagement.SceneManager.GetActiveScene().name;
+            UnityEngine.SceneManagement.SceneManager.LoadScene(currentSceneName);
+        }
+
+        public void ExitApplication() => Application.Quit();
+    }
